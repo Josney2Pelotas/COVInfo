573d13a71aab0638ad83cdfdb971b08291eaac15
Josney2Pelotas <sempre.vinicius@gmail.com>
2022-09-13 11:39:35 -0300

    removendo arquivos antigos

--- base/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from base.views import *
-
-urlpatterns = [
-    path('', MainView.as_view(), name='visao_geral_dados'),
-    path('filtros/', filtros, name='filtro_dos_filtros')
-]
